938e33f16ce4d3325ed004b0ec5dabdf623305ce
NavodContrath <navod.contrath@gmail.com>
2025-02-14 13:57:40 +0100

    add functions for random and sum

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -6,4 +6,20 @@ L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un n
 - function that sums the number and tell IF isEvenorOdd
 - results
 */
+const userNumber = Number(prompt("type your number choose from 1 to 5"))
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+const randomNumber = getRandomNumber(1, 5)
+console.log(randomNumber);
+function sum(num1, num2) {
+    return num1 + num2
+}
+const summedNumbers = sum(userNumber, randomNumber)
+console.log(summedNumbers);
+
+
+
+
+
 
