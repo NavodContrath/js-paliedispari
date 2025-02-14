6471594f97c9aed0d076345c9339a0ad58656bc3
NavodContrath <navod.contrath@gmail.com>
2025-02-14 14:51:24 +0100

    add function for plaindrome

--- palindromo/main.js
+++ palindromo/main.js
@@ -6,3 +6,15 @@ Chiedere all’utente di inserire una parola Creare una funzione per capire se l
 - function for palindrome (for loop with length to analyze the word + IF to check each index of the first half of the string and compare it with tha second)
 - const with the value(result)
 */
+const userWord = prompt("Type here uor word!")
+function isPalindrome(word) {
+    for (let i = 0; i < word.length / 2; i++) {
+        if (word[i] !== word[word.length - 1 - i]) {
+            return "It's not a palindrome!"
+        } else {
+            return "It's palindrome!"
+        }
+    }
+}
+const result = isPalindrome(userWord)
+console.log(result);
