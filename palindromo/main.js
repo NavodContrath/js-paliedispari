56b140bdaac035c3578395412146d4ddfcd0f7e2
NavodContrath <navod.contrath@gmail.com>
2025-02-14 14:33:48 +0100

    palindrome task

---
+++ palindromo/main.js
@@ -0,0 +1,8 @@
+/* Palidroma
+Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma */
+
+/*SUMMARY 
+- prompt for userWord
+- function for palindrome (for loop with length to analyze the word + IF to check each index of the first half of the string and compare it with tha second)
+- const with the value(result)
+*/
